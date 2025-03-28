c739cb5da211f3176d2f28d47a4e340415b32bd7
Chris Smith <chlsmith@gmail.com>
2025-03-28 07:13:09 -0400

    commit all

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "name" {
+  value = azurerm_resource_group.deployment.name
+}
+
+output "id" {
+  value = azurerm_resource_group.deployment.id
+}
+
+output "location" {
+  value = azurerm_resource_group.deployment.location
+}
+
+output "tags" {
+  value = azurerm_resource_group.deployment.tags
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "resource_group_name" {}
+variable "location" {
+  default = "East US 2"
+}
